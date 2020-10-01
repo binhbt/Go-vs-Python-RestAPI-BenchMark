62ec11ee1e502c49273a22c1da09daca20c21d3e
BinhBT <binhbt@sigma-solutions.eu>
2020-10-01 17:13:53 +0700

    abc

--- go-app/task.go
+++ go-app/task.go
@@ -1,10 +1,8 @@
 package main
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 // Task - Model of a basic task
 type Task struct {
-	ID    primitive.ObjectID
+	// ID    primitive.ObjectID
 	Title string
 	Body  string
 }
